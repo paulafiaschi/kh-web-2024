55d32e4633a4b79efd79187af156a2c103bb9631
paulafiaschi <pau.fiaschi@gmail.com>
2025-08-06 11:02:43 +0200

    training times - outdoor

--- app/teams/page.jsx
+++ app/teams/page.jsx
@@ -19,7 +19,8 @@ export default function Teams() {
         <TeamCard
           team="Women's"
           image="ladies2023.jpg"
-          time="Mon 20:30 - 22:00 | Wed 19:00 - 20:30"
+          // time="Mon 20:30 - 22:00 | Wed 19:00 - 20:30"
+          time="Tue & Thu 18:30 - 20:00"
           name="Fay"
           contact="faybrasser@gmail.com"
           form="https://www.holdsport.dk/team_invitation/139527/bfab9e39d968a64582c573e3995632a876a31dfa?fbclid=IwAR2LA-kzYiygh4FoXHkjDBBFvZfza0qXiDqaXLWaRtGN78zYa1QONDSwRrE"
@@ -28,7 +29,8 @@ export default function Teams() {
         <TeamCard
           team="Men's"
           image="men-2024.jpg"
-          time="Mon and Wed 20:30 - 22:00"
+          // time="Mon and Wed 20:30 - 22:00"
+          time="Tue & Thu 20:00 - 22:00"
           name="Barnes"
           contact="j.luke@gmx.com"
           form="https://www.holdsport.dk/team_invitation/139528/4fad7b974cc5ac639e59d3e3cda15986fd1d10a9?fbclid=IwAR2eenwpH8YeicDxjp6WQrWqrj3sWTgLw3mnXY0XMNDYPR5xTEj6PpKcUmQ"
@@ -37,7 +39,8 @@ export default function Teams() {
         <TeamCard
           team="Youth"
           image="youth.jpg"
-          time="Wed 18:00 - 19:00"
+          // time="Wed 18:00 - 19:00"
+          time="Tue & Thu 17:30 - 18:30"
           name="Morten"
           contact="mortenreynhard@gmail.com"
           form="https://www.holdsport.dk/team_invitation/139531/dcdb16a148c7b193ea7c18ba7aa2901adfd4c9df?fbclid=IwAR3dFGEzoji_hJ9yT9hPKSNVZUjL7rLR9SkByOq3vu6Aovi1lyf8dqBLtDg"
@@ -49,7 +52,7 @@ export default function Teams() {
           time="Thu 18:30 - 20:00"
           name="Tobias"
           contact="khformand@gmail.com"
-          form="https://holdsport.dk/team_invitation/139537/15de952cacd5e037a09e931b5c6740b54d43a3d2"
+          form="https://holdsport.dk/team_invitation/153830/9bd4b1fbf7f7e64a72dd7312a2cc9183b80679c6"
           phone="6060 8342"
         />
       </section>
